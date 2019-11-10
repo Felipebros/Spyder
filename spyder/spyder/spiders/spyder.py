56119c8f89fdfe73679750b00db5ed29fe02ea00
felipebros <felipeiturama@gmail.com>
2019-11-10 18:53:49 -0300

    Adicionado yield

--- spyder/spyder/spiders/spyder.py
+++ spyder/spyder/spiders/spyder.py
@@ -14,23 +14,23 @@ class Spyder(scrapy.Spider):
         imoveis = response.xpath('//div[@id = "property-listing"]//div[@class'\
             ' = "row"]//div[re:test(@class, "item")]')
         for imovel in imoveis:
-            id_xpath = imovel.xpath('.//div[@class = "info"]//p[@class = "cor'\
-                'ta_desc"]//strong/text()').get()
-
             price_2_xpath = imovel.xpath(
                 './/div[@class = "price"]//span[2]/text()').get()
-            price_1_xpath = imovel.xpath(
-                './/div[@class = "price"]//span/text()').get()
             if price_2_xpath != None:
                 price_xpath = self.desmascarar_moeda(price_2_xpath)
             else:
+                price_1_xpath = imovel.xpath(
+                    './/div[@class = "price"]//span/text()').get()
                 price_xpath = self.desmascarar_moeda(price_1_xpath)
-            
-            area_xpath = self.desmascarar_area(imovel.xpath(
-                './/div[@class = "info"]//ul[@class = "amenities"]//li/text()'
-            ).get())
-            
-            print(dict(id=id_xpath, price=price_xpath, area=area_xpath))
+
+            yield {
+                'price': price_xpath,
+                'area': self.desmascarar_area(imovel.xpath(
+                    './/div[@class = "info"]//ul[@class = "amenities"]//li/te'\
+                    'xt()').get()),
+                'id': imovel.xpath('.//div[@class = "info"]//p[@class = "cor'\
+                    'ta_desc"]//strong/text()').get(),
+            }
 
     def desmascarar_moeda(self, valor):
         return Decimal(float(valor.replace('R$', '').replace('.', '').replace(
